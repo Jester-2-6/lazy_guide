c72cebc907861236bcef4d9fbe3e0df73c5ebbed
jester-2-6 <cwimalasuriya@gmail.com>
2022-02-04 13:25:43 +0530

    added reqs

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Request, Response
 
 application = FastAPI(debug=True)
 
-@app.post("/api/v1/echo/")
+@application.post("/api/v1/echo/")
 async def echo(request: Request):
     body = await request.body()
     return Response(body)
