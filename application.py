4aba710bb7cfa10c3bedff7a6defa100925ee4b1
jester-2-6 <cwimalasuriya@gmail.com>
2022-02-04 13:44:13 +0530

    add cors

--- application.py
+++ application.py
@@ -1,7 +1,20 @@
 import uvicorn
 from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(debug=True)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.post('/')
+async def root():
+  return 'Home page'
 
 @app.post("/api/v1/echo/")
 async def echo(request: Request):
